d58664facee6507b061afbec99925cf0b146f33b
Dongcan Jiang <dongcan.jiang@gmail.com>
2015-02-06 22:23:24 +0800

    repair DisjunctionScorer.h

--- scorer/DisjunctionScorer.h
+++ scorer/DisjunctionScorer.h
@@ -1,64 +1,44 @@
 #ifndef DISJUNCTIONSCORER_H_INCLUDED
 #define DISJUNCTIONSCORER_H_INCLUDED
 
-class scorerDocID{
-public:
-    int scorerID;
-    int preDocID;
-    scorerDocID(int s, int d) {
-        scorerID = s;
-        preDocID = d;
-    }
-};
 
 class DisjunctionScorer: public Scorer{
 public:
     vector<shared_ptr<Scorer>> scorers;
-    vector<scorerDocID> sdHeap;
+    static bool greaterDocID (shared_ptr<Scorer> a, shared_ptr<Scorer> b) {
+        return b->doc() < a->doc();
+    };
+
     DisjunctionScorer(vector<shared_ptr<Scorer>> & scorers) {
         assert(scorers.size() > 1);
         this->scorers = scorers;
-        for(size_t i = 0; i < scorers.size(); i++) {
-            if(scorers[i]->next() < DOC_EXHAUSTED) {
-                sdHeap.push_back(scorerDocID(i,scorers[i]->doc()));
-            }
-        }
-        make_heap(sdHeap.begin(), sdHeap.end(),
-                    [](scorerDocID &a, scorerDocID &b) {
-                        return b.preDocID < a.preDocID;
-                    }
-        );
-    }
-    int score() {}
-    int next() {
-        int minID = sdHeap[0].preDocID;
-        if(sdHeap.size() == 0)
-            return docID = DOC_EXHAUSTED;
-        do{
-            int sc = sdHeap[0].scorerID;
-            pop_heap(sdHeap.begin(), sdHeap.end(),
-                        [](scorerDocID &a, scorerDocID &b) {
-                            return b.preDocID < a.preDocID;
-                        }
-            );
-            sdHeap.pop_back();
-            if(scorers[sc]->next() < DOC_EXHAUSTED) {
-                sdHeap.push_back(scorerDocID(sc,scorers[sc]->doc()));
-                push_heap(sdHeap.begin(), sdHeap.end(),
-                        [](scorerDocID &a, scorerDocID &b) {
-                            return b.preDocID < a.preDocID;
-                        }
-                );
-            }
-        }while(sdHeap.size() !=0 && sdHeap[0].preDocID == minID);
-        return docID = minID;
-    }
-
-    int cost() {
         scost = 0;
         for(auto s : scorers) {
             scost += s->cost();
         }
+        for(auto s : scorers)
+            s->next();
+        make_heap(scorers.begin(), scorers.end(), greaterDocID);
+    }
+    int score() {}
+    int next() {
+        int minID;
+        if(scorers.size() == 0)
+            return docID = DOC_EXHAUSTED;
+        minID = scorers[0]->doc();
+        do{
+            shared_ptr<Scorer> sc = scorers[0];
+            pop_heap(scorers.begin(), scorers.end(), greaterDocID);
+            scorers.pop_back();
+            if(sc->next() < DOC_EXHAUSTED) {
+                scorers.push_back(sc);
+                push_heap(scorers.begin(), scorers.end(), greaterDocID);
+            }
+        }while(scorers.size() !=0 && scorers[0]->doc() == minID);
+        return docID = minID;
+    }
+
+    int cost() {
         return scost;
     }
     ~DisjunctionScorer() {}
